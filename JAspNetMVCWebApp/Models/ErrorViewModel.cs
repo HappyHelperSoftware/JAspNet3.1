876fcd68991562d8051850c6d42c72e9fa4c9670
Joseph Patrick Donnelly <JosephPatrickDonnelly@gmail.com>
2022-01-01 16:06:00 -0800

    Initial save of a ASP.Net Core 3.1 MVC Application

---
+++ JAspNetMVCWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JAspNetMVCWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
